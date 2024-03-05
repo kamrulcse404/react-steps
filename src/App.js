32243eaf80ec5ea8d51a6758defa84fb5d2a2998
kamrulcse404 <kamrul.h01960@gmail.com>
2024-03-05 20:25:13 +0600

    update state based on current state

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ export default function App() {
 
   function handlePrevious(){
     if (step > 1) {
-      setStep(step - 1);
+      setStep(s => s - 1);
     }
   }
 
   function handleNext(){
     if (step < 3) {
-      setStep(step + 1);
+      setStep(s => s + 1);
     }
   }
 
@@ -28,7 +28,7 @@ export default function App() {
   return (
     <>
 
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      <button className="close" onClick={() => setIsOpen(is => !is)}>&times;</button>
 
       { isOpen && (
       <div className="steps">
